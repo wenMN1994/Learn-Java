f625e4ee8309fa5c9d3bbc900ad35ce190b50f98
DragonWen <18475536452@163.com>
2021-03-07 11:41:10 +0800

    ..

---
+++ src/com/dragon/basics/MyClass.java
@@ -0,0 +1,17 @@
+package com.dragon.basics;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/9/11 21:45
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public abstract class MyClass {
+    public int constant = 5;
+//    public abstract void  method(int a);
+//    public int method();
+//    public abstract void anotherMethod(){}
+    public void method(){}
+}
---
+++ src/com/dragon/basics/Test0911.java
@@ -0,0 +1,25 @@
+package com.dragon.basics;
+
+import java.util.Scanner;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/9/11 20:02
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class Test0911 {
+
+    public static void main(String[] args) {
+        Boolean b = new Boolean("abcd");
+        int x = (int)(1.23);
+        byte  e = 127;
+        float f = 123;
+        System.out.println(b);
+        System.out.println(x);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
